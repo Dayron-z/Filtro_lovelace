98013d81d447e24584314e3c3c3838d674e91b0d
Dayron-z <zbastian.zapata@gmail.com>
2024-06-05 06:11:40 -0500

    Cambios

--- src/main/java/com/example/filtro/FiltroApplication.java
+++ src/main/java/com/example/filtro/FiltroApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FiltroApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(FiltroApplication.class, args);
 	}
