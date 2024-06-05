cbfc6a23621a218c269a2bfe81a1f4f342577734
Dayron-z <zbastian.zapata@gmail.com>
2024-06-05 13:51:08 -0500

    Lesson listo

--- src/main/java/com/example/filtro/infrastructure/abstract_services/ILessonService.java
+++ src/main/java/com/example/filtro/infrastructure/abstract_services/ILessonService.java
@@ -1,7 +1,9 @@
 package com.example.filtro.infrastructure.abstract_services;
 
+import com.example.filtro.api.dto.request.custom_request.UpdatedLessonRequest;
 import com.example.filtro.api.dto.request.used_request.LessonRequest;
 import com.example.filtro.api.dto.response.used_response.LessonResponse;
 
 public interface ILessonService extends CrudService<LessonRequest, LessonResponse, Long> {
+    public LessonResponse customUpdate(UpdatedLessonRequest request, Long id);
 }
